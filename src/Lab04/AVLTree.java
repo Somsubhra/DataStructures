176fb5221bd70cf70da4d489d223c5d84d4ce258
somsubhra <somsubhra.bairi@gmail.com>
2012-10-26 11:19:37 +0530

    latest commit on lab4

--- src/Lab04/AVLTree.java
+++ src/Lab04/AVLTree.java
@@ -1,12 +1,11 @@
 /**
  * Implementation of AVL Tree.
+ * 
  * @author Somsubhra
- *
+ * 
  */
-public class AVLTree extends BSTree{
-	
-	
-	
+public class AVLTree extends BSTree {
+
 	/**
 	 * Inserts a new item into the AVL Tree.
 	 * 
@@ -23,8 +22,8 @@ public class AVLTree extends BSTree{
 
 			this.root = newNode;
 
-		} 
-		
+		}
+
 		else {
 			BSTNode current = root;
 			BSTNode parent = null;
@@ -43,8 +42,8 @@ public class AVLTree extends BSTree{
 						newNode.parent = parent;
 						break;
 					}
-				} 
-				
+				}
+
 				else {
 
 					current = current.rightChild;
@@ -58,50 +57,54 @@ public class AVLTree extends BSTree{
 				}
 			}
 		}
-		
+
 		balance(newNode);
 	}
-	
-	
-	private void balance(BSTNode node){
-		
+
+	/**
+	 * Balances the AVL Tree along a given node.
+	 * 
+	 * @param node
+	 *            The node along which balancing is to be done
+	 */
+	private void balance(BSTNode node) {
+
 		node = node.parent;
-		
-		while(node!=null){
-			if(this.balanceFactor(node)==-2){
-				
-				if(this.balanceFactor(node.rightChild)==-1){
-					
+
+		while (node != null) {
+			if (this.balanceFactor(node) == -2) {
+
+				if (this.balanceFactor(node.rightChild) == -1) {
+
 					rotateLeft(node);
 					break;
 				}
-				
-				if(this.balanceFactor(node.rightChild)==1){
-					
+
+				if (this.balanceFactor(node.rightChild) == 1) {
+
 					rotateRight(node.rightChild);
 					rotateLeft(node);
 					break;
 				}
 			}
-			
-			if(this.balanceFactor(node)==2){
-				
-				if(this.balanceFactor(node.leftChild)==1){
-					
+
+			if (this.balanceFactor(node) == 2) {
+
+				if (this.balanceFactor(node.leftChild) == 1) {
+
 					rotateRight(node);
 					break;
 				}
-				
-				if(this.balanceFactor(node.leftChild)==-1){
+
+				if (this.balanceFactor(node.leftChild) == -1) {
 					rotateLeft(node.leftChild);
 					rotateRight(node);
 					break;
 				}
 			}
-			
+
 			node = node.parent;
 		}
 	}
-	
-	
+
 }
